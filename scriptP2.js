d33341d62e1c0d1de6e3bd020e18ef5f04ff9ead
JuanMiChee <harringtonjuan@outlook.com>
2021-11-24 19:50:01 -0400

    function parametter remake, making interactive code

--- scriptP2.js
+++ scriptP2.js
@@ -2,27 +2,25 @@
 
 
 const baseImgUrl= "http://image.tmdb.org/t/p/";
-const baseUrl = "https://api.themoviedb.org/3/movie/"
+const baseUrl = "https://api.themoviedb.org/3/movie/";
 const api_key = '60586031e0e21153f7f67ca901fabc85';
+const imgBaseUrl = 'https://image.tmdb.org/t/p/w154///';
 
 const popularRuta = 'popular'
 const latestRuta = 'latest'
 const upcomingRuta = 'upcoming'
 const topRatedRuta = 'top_rated'
 
-makeUrl(popularRuta);
-makeUrl(latestRuta);
-makeUrl(upcomingRuta);
-makeUrl(topRatedRuta);
+function makeUrl(ruta, base){ 
 
-function makeUrl(ruta){
-const myUrlWithParams = new URL(ruta, baseUrl); 
+const myUrlWithParams = new URL (ruta, base);
+// console.log(mapPopular)
 
 myUrlWithParams.searchParams.append("language", "en-US");
 myUrlWithParams.searchParams.append("api_key", api_key);
 myUrlWithParams.searchParams.append("page", "1");
 
-console.log(myUrlWithParams.href);
+// console.log(myUrlWithParams.href);
 return myUrlWithParams.href;
 }
 
@@ -32,33 +30,29 @@ async function fetchMovies (url){
     return response.json();
 }
 
-//dibujo de las peliculas populares del main
-
-fetchMovies(makeUrl(topRatedRuta)).then(jsonPopularProcessing);
+fetchMovies(makeUrl(topRatedRuta, baseUrl)).then(jsonPopularProcessing);
 
 
 function jsonPopularProcessing(jsonPopular) {
   let moviesFilter = jsonPopular.results.filter(function(result){
       return result.original_language === 'en'
   })   
+   
   let movieMap = jsonPopular.results.map(mapMovie);
-  
   movieMap.forEach(drawPopularMovie);
   console.log(moviesFilter);
 }
 
-//dibujo de las peliculas populares del main
-
 function getLastestMoviesButton(){
-    fetchMovies(makeUrl(latestRuta)).then(jsonProcessing);
+    fetchMovies(makeUrl(latestRuta, baseUrl)).then(jsonProcessing);
 }
 
 function getPopularMoviesButton(){
-    fetchMovies(makeUrl(popularRuta)).then(jsonProcessing);
+    fetchMovies(makeUrl(popularRuta, baseUrl)).then(jsonProcessing);
 }
 
 function getUpcommingMoviesButton(){
-    fetchMovies(makeUrl(upcomingRuta)).then(jsonProcessing);
+    fetchMovies(makeUrl(upcomingRuta, baseUrl)).then(jsonProcessing);
 }
 
 function jsonProcessing(json) {
@@ -66,19 +60,23 @@ function jsonProcessing(json) {
         return result.original_language === 'en'
     })   
     let movieMap = json.results.map(mapMovie);
-    console.log(moviesFilter)
     movieMap.forEach(drawAllMovie);
+    console.log(moviesFilter);
+    
   }
 
 function mapMovie(movieJson) {
+    console.log(movieJson.poster_path);
+    let path = '/t/p/w154///';
+    let makingUrl = makeUrl(path + movieJson.poster_path, imgBaseUrl);
+   
     return {
         title: movieJson.title,
-        imgUrl: 'https://image.tmdb.org/t/p/w154//' + movieJson.poster_path +'?'+ api_key,
-        overview: movieJson.overview,      
+        imgUrl: makingUrl,
+        overview: movieJson.overview,            
     }
 }
 
-// var htmlImgId = 'movies-img-box';
 var htmlPopularId = 'popular-movies-box';
 var htmlContainerId = 'movies-container';
 
@@ -86,7 +84,7 @@ function drawMovie(movie, htmlId) {
     let moviesContainer = document.getElementById(htmlId);
     let moviesImgBox = document.createElement('div');
 
-    let movieTitleElement = document.createElement('h3');
+    let movieTitleElement = document.createElement('h4');
     movieTitleElement.innerHTML = movie.title;
 
     let movieImageElement = document.createElement('img');                                    
